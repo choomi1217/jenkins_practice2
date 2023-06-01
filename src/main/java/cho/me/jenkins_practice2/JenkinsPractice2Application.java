0d0140c91fca65988025793ea2eaf4280e20a7bb
ymcho <whdudal1217@naver.com>
2023-06-01 13:46:08 +0900

    init

---
+++ src/main/java/cho/me/jenkins_practice2/JenkinsPractice2Application.java
@@ -0,0 +1,13 @@
+package cho.me.jenkins_practice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPractice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cho/me/jenkins_practice2/JenkinsPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cho.me.jenkins_practice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
